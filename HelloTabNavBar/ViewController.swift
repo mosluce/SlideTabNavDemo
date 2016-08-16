d5553a6657791cfaf933c922518b5db969327b5f
mosluce <mosluce@gmail.com>
2016-08-16 10:58:21 +0800

    first commit

---
+++ HelloTabNavBar/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  HelloTabNavBar
+//
+//  Created by 默司 on 2016/8/16.
+//  Copyright © 2016年 默司. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: SlideMenuController {
+    
+    override func awakeFromNib() {
+        if let controller = self.storyboard?.instantiateViewControllerWithIdentifier("Main") {
+            self.mainViewController = controller
+        }
+        if let controller = self.storyboard?.instantiateViewControllerWithIdentifier("Left") {
+            self.leftViewController = controller
+        }
+        super.awakeFromNib()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
